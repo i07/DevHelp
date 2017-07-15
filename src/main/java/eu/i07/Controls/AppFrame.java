80b1683d9767ad775d35ebbaa9bbe3dc5a350455
Marc Donkers <marc@i07.eu>
2017-07-15 14:03:26 +0100

    update to latest version of jxBrowser

--- src/main/java/eu/i07/Controls/AppFrame.java
+++ src/main/java/eu/i07/Controls/AppFrame.java
@@ -2,14 +2,11 @@ package eu.i07.Controls;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -42,18 +39,27 @@ public class AppFrame extends JFrame implements WindowFocusListener {
     public BrowserView view = new BrowserView(browser);
     
 	public AppFrame() {
+		
 		super();
 		
 		browser.setPopupHandler(new PopupHandler() {
 		    public PopupContainer handlePopup(PopupParams params) {
+		    	
+		    	
+		    	
 		        return new PopupContainer() {
 		            public void insertBrowser(final Browser browser, final Rectangle initialBounds) {
+		            	
+		         		            	
 		                SwingUtilities.invokeLater(new Runnable() {
+		                	
 		                    @Override
 		                    public void run() {
+		                    	
 		                        BrowserView browserView = new BrowserView(browser);
+		                        
 		                        browserView.setPreferredSize(initialBounds.getSize());
-
+		                        
 		                        final JFrame frame = new JFrame("DevHelp Builder");
 		                        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		                        frame.add(browserView, BorderLayout.CENTER);
@@ -61,19 +67,20 @@ public class AppFrame extends JFrame implements WindowFocusListener {
 //		                        frame.pack();
 		                        frame.setLocation(initialBounds.getLocation());
 		                        frame.setVisible(true);
+		                         
 		                        frame.addWindowListener(new WindowAdapter() {
 		                            @Override
 		                            public void windowClosing(WindowEvent e) {
 		                                browser.dispose();
 		                            }
 		                        });
-
+		                        
 		                        browser.addDisposeListener(new DisposeListener<Browser>() {
 		                            public void onDisposed(DisposeEvent<Browser> event) {
 		                                frame.setVisible(false);
 		                            }
 		                        });
-		                        
+		                         
 		                        browser.addConsoleListener(new ConsoleListener() {
 		                		    public void onMessage(ConsoleEvent event) {
 		                		        System.out.println("Message: " + event.getMessage());
@@ -86,7 +93,7 @@ public class AppFrame extends JFrame implements WindowFocusListener {
 		        };
 		    }
 		});
-
+		 
 		setFocusable(true);
 		requestFocusInWindow(true);
 		
